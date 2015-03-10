3c0b367aab0d2574a42897f908a57f66a41398be
Sergei Vinichenko <winechess@gmail.com>
2015-03-10 13:56:07 +0300

    Add get and set modifiers Add virtual fields

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -7,9 +7,37 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    email: String,
-    username: String,
-    password: String
+    email: {
+        type: String,
+        index: true
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true
+    },
+    password: String,
+    created: {
+        type: Date,
+        default: Date.now()
+    },
+    website: {
+        type: String,
+        get: function (url) {
+
+            if (url && url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+                url = "http://" + url;
+            }
+            return url;
+        }
+    }
+
 });
+//virtual fields
+UserSchema.virtual('fullName').get(function () {
+    return this.firstName + ' ' + this.lastName;
+});
+
+UserSchema.set('toJSON', {getters: true, virtuals: true});
 
 mongoose.model('User', UserSchema);
