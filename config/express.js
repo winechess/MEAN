8ccc771b82d370591beb5b3d58c994fab29d978a
Sergei Vinichenko <winechess@gmail.com>
2015-03-10 23:32:28 +0300

    Configured pasport module

--- config/express.js
+++ config/express.js
@@ -8,6 +8,7 @@ var compress = require("compression");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var session = require('express-session');
+var passport = require('passport');
 
 module.exports = function () {
     var app = express();
@@ -32,6 +33,10 @@ module.exports = function () {
 
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
+
+    //initialize passport module
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     require('../app/routes/index.server.routes')(app);
     require('../app/routes/user.server.routes')(app);
---
+++ config/passport.js
@@ -0,0 +1,3 @@
+/**
+ * Created by vinichenkosa on 11.03.15.
+ */
